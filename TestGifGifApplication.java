894655e52bee953f50777564c24a3b6871772fa3
ys <sunnyskylove22@gmail.com>
2024-05-31 12:02:38 +0900

    test

---
+++ TestGifGifApplication.java
@@ -0,0 +1,13 @@
+package com.example.testgifgif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGifGifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGifGifApplication.class, args);
+    }
+
+}
